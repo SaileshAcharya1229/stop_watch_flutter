91a62c03aa83fc28e5890e95a17a6429dd0bee4b
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-09-19 22:33:53 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stop watch',
+      home: HomeApp(),
+    );
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1C2757),
+      
+    );
+  }
+}
