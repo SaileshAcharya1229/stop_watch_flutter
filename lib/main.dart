4b0db5da721c1bbf4fcca69f728659e1eb5b460e
SaileshAcharya1229 <saileshacharya1229@gmail.com>
2024-09-20 20:43:35 +0545

    Adding container and button

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,55 @@ class _HomeAppState extends State<HomeApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1C2757),
-      
+      backgroundColor: Color(0xFF1C2757),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  "StopWatch App",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 400.0,
+                decoration: BoxDecoration(
+                  color: Color(0xFF323F68),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () {},
+                      shape: StadiumBorder(
+                        side: BorderSide(color: Colors.blue),
+                      ),
+                      child: Text("Start"),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
